d3b301e9988a29ff9a6014e7955e329a8546b4e1
Henry Hale <henryhale@users.noreply.github.com>
2024-04-28 19:15:41 +0300

    chore: update deps

--- client/src/hash.js
+++ client/src/hash.js
@@ -1,8 +1,8 @@
-import c from "crypto-js";
+import { AES } from "crypto-js";
 
 export default function hash(s1, s2) {
     return c.HmacSHA256(s1 + s2, s2).toString();
 }
 
-export const encrypt = (msg, key) => c.AES.encrypt(msg, key).toString();
-export const decrypt = (msg, key) => c.AES.decrypt(msg, key).toString(c.enc.Utf8);
+export const encrypt = (msg, key) => AES.encrypt(msg, key).toString();
+export const decrypt = (msg, key) => AES.decrypt(msg, key).toString(c.enc.Utf8);
